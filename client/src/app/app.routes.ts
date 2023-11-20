6643ae39841f5528ee814586583d5e6ebc31e7ef
kikswosil <kikswosil@wp.pl>
2023-11-20 15:45:44 +0100

    created the first route.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  {
+    path: '/',
+    component: AppComponent,
+    title: 'Todoapp home.',
+  },
+];
