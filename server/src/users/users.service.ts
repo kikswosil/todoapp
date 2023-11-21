c8f73911ea226edc8872f3f02133ee5e8b558c4d
kikswosil <kikswosil@wp.pl>
2023-11-21 11:46:15 +0100

    replaced a placeholder array with prisma service in users.service

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from '../prisma/prisma.service';
+
 import { User } from './user.interface';
 
 @Injectable()
 export class UsersService {
-    users: User[] = [
-        {
-            id: 1,
-            name: 'kikswosil',
-            email: 'kikswosil@example.com',
-            password: 'password'
-        }
-    ];
+  //   users: User[] = [
+  //     {
+  //       id: 1,
+  //       name: 'kikswosil',
+  //       email: 'kikswosil@example.com',
+  //       password: 'password',
+  //     },
+  //   ];
 
-    async getUserByEmail(email: string): Promise<User | undefined> {
-        return this.users.find((user: User) => user.email === email);
-    }
+  constructor(private prismaService: PrismaService) {}
+
+  async getUserByEmail(email: string): Promise<User | undefined> {
+    return await this.prismaService.user.findUnique({
+      where: { email: email },
+    });
+    // return this.users.find((user: User) => user.email === email);
+  }
 }
