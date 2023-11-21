12a2b08d34516378bc5d84ae9992c253e72f3fbf
kikswosil <kikswosil@wp.pl>
2023-11-21 11:58:37 +0100

    tested if auth still works and cleaned up users.service

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -6,21 +6,11 @@ import { User } from './user.interface';
 
 @Injectable()
 export class UsersService {
-  //   users: User[] = [
-  //     {
-  //       id: 1,
-  //       name: 'kikswosil',
-  //       email: 'kikswosil@example.com',
-  //       password: 'password',
-  //     },
-  //   ];
-
   constructor(private prismaService: PrismaService) {}
 
   async getUserByEmail(email: string): Promise<User | undefined> {
     return await this.prismaService.user.findUnique({
       where: { email: email },
     });
-    // return this.users.find((user: User) => user.email === email);
   }
 }
