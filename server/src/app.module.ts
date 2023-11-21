1a4a9a5eae9a7dbf4d46c736b6f86d8ee6050119
kikswosil <kikswosil@wp.pl>
2023-11-21 11:36:33 +0100

    created prisma service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TodosModule } from './todos/todos.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [UsersModule, AuthModule, TodosModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
