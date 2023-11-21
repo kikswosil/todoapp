c0da9d537c3526886453e4959fd75eabb5e63e28
kikswosil <kikswosil@wp.pl>
2023-11-21 11:31:28 +0100

    created service and controller for Todos.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
