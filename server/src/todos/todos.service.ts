2c6504b5dc2ed7e79e04d99619b8ad28ef5cac19
kikswosil <kikswosil@wp.pl>
2023-11-22 14:36:30 +0100

    implemented delared methods

--- server/src/todos/todos.service.ts
+++ server/src/todos/todos.service.ts
@@ -3,18 +3,48 @@ import { Injectable } from '@nestjs/common';
 import { Todo } from '@prisma/client';
 
 import { PrismaService } from '../prisma/prisma.service';
+import { title } from 'process';
 
 @Injectable()
 export class TodosService {
   constructor(private prismaService: PrismaService) {}
 
-  async getTodosByUserId(userId: number): Promise<Todo[]> {}
+  async getTodosByUserId(userId: number): Promise<Todo[]> {
+    return await this.prismaService.todo.findMany({
+      where: { authorId: userId },
+    });
+  }
 
-  async getTodoById(todoId: number): Promise<Todo> {}
+  async getTodoById(todoId: number): Promise<Todo> {
+    return await this.prismaService.todo.findFirst({ where: { id: todoId } });
+  }
 
-  async insertTodo(todo: Todo) {}
+  async insertTodo(todo: Todo) {
+    return await this.prismaService.todo.create({
+      data: {
+        ...todo,
+        author: {
+          connect: {
+            where: {
+              id: todo?.authorId,
+            },
+          },
+        },
+      },
+      include: {
+        author: true,
+      },
+    });
+  }
 
-  async updateTodo(oldTodoId: number, newTodo: Todo) {}
+  async updateTodo(oldTodoId: number, newTodo: Todo) {
+    return await this.prismaService.todo.update({
+      data: newTodo,
+      where: { id: oldTodoId },
+    });
+  }
 
-  async deleteTodo(todoId: number) {}
+  async deleteTodo(todoId: number) {
+    return await this.prismaService.todo.delete({ where: { id: todoId } });
+  }
 }
