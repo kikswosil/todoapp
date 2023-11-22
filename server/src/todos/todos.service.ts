d4a20482abbcde86cc47639e2bf437e80e6632c8
kikswosil <kikswosil@wp.pl>
2023-11-22 14:16:39 +0100

    injected PrismaService into TodosService.

--- server/src/todos/todos.service.ts
+++ server/src/todos/todos.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from '../prisma/prisma.service';
+
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  constructor(private prismaService: PrismaService) {}
+}
