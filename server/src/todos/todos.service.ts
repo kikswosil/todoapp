7bb061df8a14953f9d5a6ef6068f9c5c650dfd91
kikswosil <kikswosil@wp.pl>
2023-11-22 14:22:46 +0100

    declared crud methods in the todo service.

--- server/src/todos/todos.service.ts
+++ server/src/todos/todos.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+import { Todo } from '@prisma/client';
+
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class TodosService {
   constructor(private prismaService: PrismaService) {}
+
+  async getTodosByUserId(userId: number): Promise<Todo[]> {}
+
+  async getTodoById(todoId: number): Promise<Todo> {}
+
+  async insertTodo(todo: Todo) {}
+
+  async updateTodo(oldTodoId: number, newTodo: Todo) {}
+
+  async deleteTodo(todoId: number) {}
 }
