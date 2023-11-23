7d046b4a36e57aa4421cfe5b1fce398dd2302ca6
kikswosil <kikswosil@wp.pl>
2023-11-23 18:35:32 +0100

    created login component and added it to the router

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
-}
+export class AppComponent {}
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
-    path: '/',
-    component: AppComponent,
+    path: '',
+    component: LoginComponent,
     title: 'Todoapp home.',
   },
 ];
