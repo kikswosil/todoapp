ed79efcf63d056081d1afb5f07ba29818292e34d
kikswosil <kikswosil@wp.pl>
2023-11-23 15:03:19 +0100

    implemented getTodosByUserId in todos.controller

--- server/src/todos/todos.controller.ts
+++ server/src/todos/todos.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TodosService } from './todos.service';
 
 @Controller('todos')
 export class TodosController {
   constructor(private todosService: TodosService) {}
+
+  @Get('/:userId')
+  async getTodosByUserId(@Param('userId') userId: string) {
+    return await this.todosService.getTodosByUserId(Number(userId));
+  }
 }
