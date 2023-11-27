828ab3a1273922248c44e8b589a27ea240b3544b
kikswosil <kikswosil@wp.pl>
2023-11-27 16:21:51 +0100

    added model to login form.

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-login',
+  imports: [FormsModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  model = new User(0, "", "");
+
+  onSubmit() {
+    console.log(this.model);
+  }
+}
