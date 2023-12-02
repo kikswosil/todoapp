0353e9c0a1a1d61db4578d73234b943ad7c53809
kikswosil <kikswosil@wp.pl>
2023-12-02 12:25:38 +0100

    Injected router into the logout component.

--- client/src/app/logout/logout.component.ts
+++ client/src/app/logout/logout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -14,8 +15,9 @@ import { UserService } from '../user/user.service';
   styleUrl: './logout.component.css'
 })
 export class LogoutComponent implements OnInit {
-  constructor(@Inject(UserService) private userService: UserService){}
-  ngOnInit(): void {
+  constructor(@Inject(UserService) private userService: UserService, @Inject(Router) private router: Router){}
 
+  ngOnInit(): void {
+    
   }
 }
