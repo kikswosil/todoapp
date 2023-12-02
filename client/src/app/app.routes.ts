d2b3f56adb77564cb23df9d40ea58baf7e79d44a
kikswosil <kikswosil@wp.pl>
2023-12-02 12:19:25 +0100

    added logout component to the routes.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { LogoutComponent } from './logout/logout.component';
 
 export const routes: Routes = [
   {
@@ -13,5 +14,10 @@ export const routes: Routes = [
     path: 'app',
     component: MainComponent,
     title: 'Todo app'
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+    title: 'logging out.'
   }
 ];
