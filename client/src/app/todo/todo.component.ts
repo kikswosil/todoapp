f9888b8eb6c3546dd0f2255cd1c824ebc233a781
kikswosil <kikswosil@wp.pl>
2023-12-05 18:38:15 +0100

    created a buttons list and added a way to open it

--- client/src/app/todo/todo.component.ts
+++ client/src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from './todo.interface';
 
@@ -8,12 +8,38 @@ import { Todo } from './todo.interface';
   imports: [CommonModule],
   template: `
     <div class="todo">
-      <h1>{{ todo.isDone ? '✅' : '❌' }} {{ todo.title }}</h1>
-      <div>{{ todo.details }}</div>
+      <div>
+        <h1>{{ todo.isDone ? '✅' : '❌' }} {{ todo.title }}</h1>
+        <div>{{ todo.details }}</div>
+      </div>
+      <div class="buttons">
+        <button class="list-button" (click)="toggleListOpen()">...</button>
+        <ul class="list" *ngIf="isListOpen">
+          <li (click)="markAsDone()">mark as done</li>
+          <li (click)="edit()">edit</li>
+        </ul>
+      </div>
     </div>
   `,
   styleUrl: './todo.component.css',
 })
 export class TodoComponent {
+  
   @Input({ required: true }) todo!: Todo;
+
+  isListOpen: boolean = false;
+
+  public ngAfterViewInit(): void {}
+
+  public toggleListOpen() {
+    this.isListOpen = !this.isListOpen;
+  }
+
+  public markAsDone() {
+    console.log('done')
+  }
+
+  public edit() {
+    console.log('edit')
+  }
 }
