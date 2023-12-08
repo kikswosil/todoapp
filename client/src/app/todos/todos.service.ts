f5a36e597cba18029546922c1003a5768f685e87
kikswosil <kikswosil@wp.pl>
2023-12-08 11:34:10 +0100

    injected UserService into the TodosService.

--- client/src/app/todos/todos.service.ts
+++ client/src/app/todos/todos.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-
-  constructor() { }
+  constructor(@Inject(UserService) private userService: UserService) { }
 }
